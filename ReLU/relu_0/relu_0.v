a9a9ae32ca1220feb8b35fd0ba5ca9a6ceb785fd
Chayashree-R <chayashreer3@gmail.com>
2023-09-29 11:34:21 +0530

    Add valid data feature
    Valid data was added as an input and concatenated it with the output.
    Why valid data? To ensure that the data received from the previous layer
    is correct

--- ReLU/relu_0/relu_0.v
+++ ReLU/relu_0/relu_0.v
@@ -4,25 +4,31 @@
 // Module Name: relu_0
 // Project Name: CNN Acceleration. 
 // Description: The below ReLU block replaces all negative values received as inputs by zeros.
+// Revision1: Sept 29, 2023 - Added feature is the data_valid signal.
+
 
 module relu_0(
-  input [31:0]   i_data, // Input to the Relu from previous layer
-  input          clk,
-  output [31:0]  o_data //Output of the Relu  
+  //input signed [31:0]   i_data, // Input to the Relu from previous layer
+  input [31:0]   i_data,
+  input                 clk,
+  input                 data_valid,
+  output [32:0]         o_data //Output of the Relu  
 );
-    
-  reg[31:0] out_reg;  //Internal reg to latch the output 
-    
+  reg       r_data_valid;
+  reg[31:0] out_reg;  //Internal reg to latch the output   
 always @(posedge clk) begin
-  if (i_data[31] == 1) begin
+  if (data_valid == 1) begin
+    if (i_data[31] == 1) begin
+      out_reg <= 0;
+      r_data_valid <= data_valid;
+    end else begin
+      out_reg <= i_data;
+      r_data_valid <= data_valid; 
+    end 
+    end else begin
     out_reg <= 0;
-  end else begin
-    out_reg <= i_data;
+    r_data_valid <= 0;
   end
-end    
-    assign o_data = out_reg;
-    
-    
-    
-    
+end 
+    assign o_data = {r_data_valid,out_reg};  
 endmodule

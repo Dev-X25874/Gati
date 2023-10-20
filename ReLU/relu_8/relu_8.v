a71df8b7d836d0944cb5e0169133ff14c87b1b2b
Chayashree-R <chayashreer3@gmail.com>
2023-10-20 16:46:19 +0530

    [#13]: Update relu_8 with valid
    Please enter the commit message for your changes. Lines starting

--- ReLU/relu_8/relu_8.v
+++ ReLU/relu_8/relu_8.v
@@ -5,25 +5,31 @@
 // Project Name: CNN Acceleration. 
 // Description: ReLU8: ReLU8 is a further variant that clips the output at 8 instead of 6. 
 //              It operates similarly to ReLU6 but with a different clipping threshold.
-
+// Revision1: Oct 20, 2023 - Added feature is the data_valid signal.
 
 module relu_8(
-  input[31:0]   i_data, // Input to the Relu from previous layer which is a unsigned number
+ // input signed [31:0]   i_data, // Input to the Relu from previous layer which is a unsigned number
+  input [31:0]  i_data,
   input         clk,
-  output[7:0]   o_data //Output of the Relu
+  output [8:0]  o_data, //Output of the Relu
+  input         data_valid
 );
-  reg[7:0]      out_reg = 8'd0;  //Internal reg to latch the output
-    
-always @(posedge clk) begin
-  if (i_data[31] == 1)
-    out_reg <= 0;
-  else if ((i_data > 0) && (i_data <= 8))
-    out_reg <= i_data;
-  else if ( i_data > 8)
-    out_reg <= 8'h08;
-end
-   assign o_data = out_reg;
-    
+  reg [7:0]     r_data_out = 8'd0;  //Internal reg to latch the output
+  reg           r_data_valid = 0;  
+
+  always @(posedge clk) begin
+    if (data_valid) begin
+      if ((i_data >= 0) && (i_data <= 8)) begin
+        r_data_out <= i_data;
+        r_data_valid <= data_valid;
+      end else if ( i_data > 8) begin
+        r_data_out <= 8'd8;
+        r_data_valid <= data_valid;
+      end 
+    end else begin
+      r_data_out <= 0;
+      r_data_valid <= 0;
+  end
+  end 
+   assign o_data = {r_data_valid,r_data_out};   
 endmodule
-
-

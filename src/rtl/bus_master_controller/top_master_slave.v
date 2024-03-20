464868de65e7a28092233134e21fe665cdaff956
vansh <vanshikamahendra104@gmail.com>
2024-03-20 19:06:15 +0530

    incomplete connections in top module of the design

--- src/rtl/bus_master_controller/top_master_slave.v
+++ src/rtl/bus_master_controller/top_master_slave.v
@@ -6,27 +6,48 @@ module top_master_slave #(parameter op_code_width = 4,
                 input start,
                 input clk,
                 input [(op_code_width)-1 : 0] op_code,
-                input ready,
+                //input ready,
                 output [179:0] dout_final
             );
+
+reg [144:0] dout_op_conv;
+reg [176:0] dout_op_fc;
+reg [107:0] dout_op_ob;
+reg [121:0] dout_op_tb;
+reg wr_op_conv;
+reg wr_op_fc;
+reg wr_op_ob;
+reg wr_op_tb;
+reg wr_top_master;
+reg done_op_conv;
+reg done_op_fc;
+reg done_op_ob;
+reg done_op_tb;
+reg done_top_master;
+reg [(1<<op_code_width)-1 : 0] sel_top_master;
+reg [(1<<op_code_width)-1 : 0] sel_op_conv;
+reg [(1<<op_code_width)-1 : 0] sel_op_fc;
+reg [(1<<op_code_width)-1 : 0] sel_op_ob;
+reg [(1<<op_code_width)-1 : 0] sel_op_tb;
+
 top_master top_master(
-    .din(),
-    .start(),
-    .clk(),
-    .op_code(),
-    .ready_in(),
-    .dout(),
-    .sel(),
-    .write(),
-    .done()  
+    .din(din),
+    .start(start),
+    .clk(clk),
+    .op_code(op_code),
+    .ready_in(ready),
+    .dout(dout_final),
+    .sel(sel_top_master),
+    .write(wr_top_master),
+    .done(done_top_master)  
 );
 
 OP_CONV OP_CONV(
     .din(),
-    .sel(),
-    .write(),
-    .done(),
-    .clk(),
+    .sel(sel_op_conv),
+    .write(wr_op_conv),
+    .done(done_op_conv),
+    .clk(clk),
     .opcode(),
     .IW(),
     .IH(),
@@ -43,15 +64,15 @@ OP_CONV OP_CONV(
     .kernelItr(),
     .valid(),
     .ready(),
-    .dout()
+    .dout(dout_op_conv)
 );
 
 OP_FC OP_FC(
     .din(),
-    .sel(),
-    .write(),
-    .done(),
-    .clk(),
+    .sel(sel_op_fc),
+    .write(wr_op_fc),
+    .done(done_op_fc),
+    .clk(clk),
     .valid(),
     .ready(),
     .opcode(),
@@ -64,15 +85,15 @@ OP_FC OP_FC(
     .imagedim(),
     .imageendaddr(),
     .FCbias(),
-    .dout()
+    .dout(dout_op_fc)
 );
 
 OP_Outputblock OP_Outputblock(
     .din(),
-    .sel(),
-    .write(),
-    .done(),
-    .clk(),
+    .sel(sel_op_ob),
+    .write(wr_op_ob),
+    .done(done_op_ob),
+    .clk(clk),
     .valid(),
     .ready(),
     .opcode(),
@@ -81,15 +102,15 @@ OP_Outputblock OP_Outputblock(
     .channelItr(),
     .kernelItr(),
     .imagedim(),
-    .dout()
+    .dout(dout_op_ob)
 );
 
 OP_Tailblock OP_Tailblock(
     .din(),
-    .sel(),
-    .write(),
-    .done(),
-    .clk(),
+    .sel(sel_op_tb),
+    .write(wr_op_tb),
+    .done(done_op_tb),
+    .clk(clk),
     .ready(),
     .valid(),
     .opcode(),
@@ -104,9 +125,33 @@ OP_Tailblock OP_Tailblock(
     .poolheight(),
     .poolstride(),
     .poolpadding(),
-    .dout()
+    .dout(dout_op_tb)
 );
 
-
-
+case(opcode) 
+4'b000: begin
+    dout_final <= dout_op_conv;
+    sel_op_conv <= sel_top_master;
+    wr_op_conv <= wr_top_master;
+    done_op_conv <= done_top_master;
+end
+4'b001: begin
+    dout_final <= dout_op_fc;
+    sel_op_fc <= sel_top_master;
+    wr_op_fc <= wr_top_master;
+    done_op_fc <= done_top_master;
+end
+4'b011: begin
+    dout_final <= dout_op_ob;
+    sel_op_ob <= sel_top_master;
+    wr_op_ob <= wr_top_master;
+    done_op_ob <= done_top_master;
+end
+4'b101: begin
+    dout_final <= dout_op_tb;
+    sel_op_tb <= sel_top_master;
+    wr_op_tb <= wr_top_master;
+    done_op_tb <= done_top_master;
+end
+endcase
 endmodule

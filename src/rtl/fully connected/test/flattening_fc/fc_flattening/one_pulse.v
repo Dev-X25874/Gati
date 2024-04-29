fb4b0e601f6be3264d900e3a41f056ccd1e5122a
roshnijain01 <jainroshnii016@gmail.com>
2024-04-29 11:17:34 +0530

    fc_flattening: Parameterize flattening for nxn image dimension

---
+++ src/rtl/fully connected/test/flattening_fc/fc_flattening/one_pulse.v
@@ -0,0 +1,25 @@
+//Gives output of one cycle of incoming trigger signal
+module one_pulse(
+    input a,
+    input rst,
+    input clk,
+    output reg b
+);
+reg temp_a,temp_b;
+
+always @(a) begin
+    if(rst)
+        temp_a <= 0;
+    else
+        temp_a<=a;
+end
+always @(posedge clk) begin
+    if(rst)begin
+        temp_b <= 0;
+        b <= 0;
+    end else begin
+        temp_b <= temp_a;
+        b <= (temp_a & (~temp_b));
+    end
+end
+endmodule
---
+++ src/rtl/fully connected/test/flattening_fc/fc_flattening/output_mux.v
@@ -0,0 +1,58 @@
+module output_mux#(
+    parameter N_BANK = 4,
+    parameter N_BRAM = 8,
+    parameter W_DATA = 8
+)(
+    input clk,
+    input rst,
+    input [N_BRAM-1 : 0] i_rden,
+    input [N_BANK-1 : 0] i_bank_en,
+    input [(N_BANK * (N_BRAM * W_DATA))-1 : 0] i_data,
+    output [W_DATA-1 : 0] o_data,
+    output data_valid
+);
+reg dv = 0;
+reg [W_DATA-1 : 0] data = 0;
+assign o_data = data;
+assign data_valid = dv;
+localparam BIN_WIDTH_RDEN = $clog2(N_BRAM);
+localparam BIN_WIDTH_BANK_EN = $clog2(N_BANK);
+localparam BIN_WIDTH = BIN_WIDTH_RDEN + BIN_WIDTH_BANK_EN;
+
+wire [BIN_WIDTH_RDEN-1 : 0] read_en_bin;
+onehot_to_bin#(
+    .ONEHOT_WIDTH(N_BRAM)
+)read_en(
+    .onehot(i_rden),
+    .bin(read_en_bin)
+);
+
+wire [BIN_WIDTH_BANK_EN-1 : 0] bank_en_bin;
+onehot_to_bin#(
+    .ONEHOT_WIDTH(N_BANK)
+)bank_en(
+    .onehot(i_bank_en),
+    .bin(bank_en_bin)
+);
+
+wire [BIN_WIDTH-1 : 0] select;
+assign select = {bank_en_bin, read_en_bin};
+
+// assign o_data = rst ? 0 : ((i_rden != 0) ? (i_data[((W_DATA) * ((N_BRAM * N_BANK) - select))-1 -: W_DATA]) : 0);
+// assign data_valid = rst ? 0 : ((i_rden != 0) ? 1'b1 : 1'b0);
+
+always @(posedge clk) begin
+    if(rst)begin
+        data <= 0;
+    end else begin
+            if(i_rden!=0)begin
+                data <= i_data[((W_DATA) * ((N_BRAM * N_BANK) - select))-1 -: W_DATA];
+                dv <= 1'b1;
+            end else begin
+                data <= data;
+                dv <= 1'b0;
+            end
+        end
+end
+    
+endmodule

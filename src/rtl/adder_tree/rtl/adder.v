7d64660a6936694bf075ed56de2991215dc8edec
vansh <vanshikamahendra104@gmail.com>
2024-05-04 18:08:03 +0530

    parameterized the rtl files

--- src/rtl/adder_tree/rtl/adder.v
+++ src/rtl/adder_tree/rtl/adder.v
@@ -1,29 +1,34 @@
 //this module adds the outputs coming from 2 consecutive engines
 
-module adder # (parameter WIDTH=20)(
-    input clk,
-    input rst,
-    input valid_in,
-    input [WIDTH-1:0] first_k, //input from an engine
-    input [WIDTH-1:0] second_k, //input from another engine
-    output reg valid = 0,
-    output reg [WIDTH-1:0] result = 0
+module adder #(
+    parameter WIDTH   = 20
+  , parameter O_WIDTH = 21
+) (
+  input clk,
+  input rst,
+  input valid_in_1,
+  input valid_in_2,
+  input [WIDTH-1:0] first_k,
+  input [WIDTH-1:0] second_k,
+  output reg valid = 0,
+  output reg [O_WIDTH-1 : 0] result = 0
 );
+reg [1:0] state = 0;
 
 always @(posedge clk) begin
-    if(rst) begin
-        if(valid_in) begin
-            result <= first_k + second_k; //adder tree result
-            valid <= 1'b1;
-        end
-        else begin
-            result <= result;
-            valid <= 1'b0;
-        end 
+  if (~rst) begin
+    result <= 0;
+    valid  <= 1'b0;
+  end 
+  else begin
+    
+    if (valid_in_1 & valid_in_2) begin
+      result <= first_k + second_k;
+      valid  <= 1'b1;
+    end else begin
+      result <= result;
+      valid  <= 1'b0;
     end
-    else begin
-        result <= 0;
-        valid <= 1'b0;
-    end
+  end
 end
 endmodule

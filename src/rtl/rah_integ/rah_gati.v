e65ff7e7a62990b87cab3a5daffbb7f6a5967aa7
SHWET DHAMELIYA <shwet638@gmail.com>
2024-05-15 17:10:25 +0530

    rah cntrl

---
+++ src/rtl/rah_integ/rah_gati.v
@@ -0,0 +1,37 @@
+module rah_gati
+	(
+		input clk,
+		input empty,
+		input [47:0] data,
+		output rden
+	)
+	
+	reg [47:0] r_data=0;
+	reg valid_data=0;
+	
+	always @ (posedge clk) begin 
+		
+		if(!empty) begin 
+			rden<=1;
+		end
+		else begin 
+			rden<=0;
+		end
+
+		if(rden)  
+			valid_data<=1;
+		else
+			valid_data<=0;
+		
+		if(valid_data) 
+			r_data<=data;
+	end
+
+
+	
+
+
+//////////////////////////////// gati module instatiation
+
+///////////////////////////////	
+

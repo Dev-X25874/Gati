1aa83123e91fbf655b6da5418f74e7c505c0f997
Shreeyash Pandey <shreeyash335@gmail.com>
2024-05-22 17:03:39 +0530

    feat(docs): add cpp defines
    Signed-off-by: Shreeyash Pandey <shreeyash335@gmail.com>

---
+++ docs/source/instructions/gen_cpp_defines.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+
+file_name=$1
+op_name="$(head -n 1 $file_name | cut -d ":" -f 1)"
+op_code="$(head -n 1 $file_name | cut -d ":" -f 2)"
+printf "\t#define OP_$op_name $op_code\n"
+
+lower_bound=0
+upper_bound=0
+while read line; do
+  name="$(echo $line | cut -d ":" -f 1)"
+  upper_bound="$(( upper_bound + $(echo $line | cut -d ":" -f 2) ))"
+  printf "\t#define "$name"_LOW "$lower_bound"\n"
+  printf "\t#define "$name"_HIGH "$(( upper_bound - 1 ))"\n"
+  lower_bound=$upper_bound
+done <<< $(tail -n +2 $file_name)
+echo 

960b1fd79cd171de877ba48af623f7c7e6615b07
SHWET DHAMELIYA <shwet638@gmail.com>
2024-05-27 11:35:57 +0530

    added mipi ctrl
    Signed-off-by: SHWET DHAMELIYA <shwet638@gmail.com>

--- src/rtl/rah_integ/rah_gati.v
+++ src/rtl/rah_integ/rah_gati.v
@@ -1,6 +1,86 @@
-module rah_gati
+module rah_gati #(
+//globalutput [(OP_FIFO*DATA_WIDTH_OB)-1:0] op_dram_fifo
+	 parameter INST_QUEUE_DEPTH = 512,
+	 parameter DRAM_IMG_FIFO_DEPTH = 512,
+	 parameter IM2COL_FIFO_DEPTH = 1024,
+	 parameter WEIGHT_FIFO_DEPTH = 512,
+	 parameter PSUM_FIFO_DEPTH = 8192,
+	 parameter ACC_FIFO_DEPTH = 512,
+	 parameter BIAS_FIFO_DEPTH = 512, //For both conv and FC
+	 parameter OP_WRITE_FIFO_DEPTH = 2048,
+     //Default burst lenghts for various memory request controllers
+	 parameter CONFIG_REQ_BLEN = 7,
+	 parameter IMG_REQ_BLEN = 15,
+	 parameter WEIGHT_REQ_BLEN = 15,
+     parameter ACC_REQ_BLEN = 15,
+     parameter OP_WRITE_REQ_ACC_BLEN = 31, //burst length for writng accumulants (32-bit) into the DRAM
+     parameter OP_WRITE_REQ_QUA_BLEN = 15, //burst length for writng quantized wire (8-bit) into the DRAM
+
+     parameter CONFIG_FIFO_OCCUPANCY = 10,
+     parameter LAYERCNT_WIDTH = 12,
+     //parameters related to DRAM controller
+     parameter NUM_PORTS = 4, //Number of read and write requestors
+     /*
+     parameter PORT_ID_WIDTH = 4, 
+     parameter ADDR_WIDTH_MEM_CTRL = 8, //address width to the wire of DRAM ctrl from operator mem request ctrlers
+     parameter PORT_ID = {4'd10, 4'd11, 4'd12, 4'd13, 4'd14} // Depends on the port_id_width and number of requestors
+     */
+
+     parameter AXI_DATA_BYTES = 32,  // Axi Data width = 256 bit
+     parameter AXI_ADDR_W = 32,          // Axi Address width
+     parameter BURST_LENGTH_WIDTH =8,
+    
+     parameter NUM_INSTRUCTIONS = 4,
+     parameter INST_W = 256,
+
+     parameter OPCODE_WIDTH = 4,
+     parameter N_SA = NSA_DSP + NSA_LUT,
+     parameter DATA_WIDTH = 8,
+     parameter COL_SA = 4,
+     parameter COL_FC = 32,
+     parameter W_QUANT_SHIFT = 5,
+     parameter W_QUANT_SCALE = 16,
+     parameter ROW = 9,
+     parameter W_PSUM = 20,
+     parameter MOD1 = 2,
+     parameter MOD2 = 8,
+     parameter DATA_WIDTH_OB = 32,
+     parameter IMAGE_DIM = 224,
+     parameter SHFT_REG_X = 4, // Number of shift register blocks
+     parameter BIAS_FIFO = 8, // Number of bias FIFOs
+     parameter OP_FIFO = 8,  // Number of wire write FIFOs
+     parameter NSA_DSP = 4, 
+     parameter N_FC_MUX = 4,
+     parameter NO_PORT_FC = 8,
+     parameter RELU_CLIP_WIDTH = 8,
+     parameter NSA_LUT = 0,
+     parameter BIAS_FIFO_FC = 32, // Number of FC bias FIFOs
+     parameter NO_PORT_VA = 2,
+     parameter NO_PORT_BAC = 2,
+     parameter NO_PORT_BAFC = 8,
+     parameter POP_THRESHOLD = 5,
+     parameter I_SIZE_WIDTH=20, // bit width of wire image dimension
+     parameter ACC_DW = 32,
+     parameter N_BANK = 4,
+     parameter N_BRAM = 8,
+     parameter W_FC_RW_COUNTER = 10, //width of fc r/w address counter
+     parameter FC_BRAM_DEPTH = 1024,
+     parameter W_KERNEL_CNT = 16,
+     parameter W_IMAG_DIM = 20,
+	 parameter ACC_DATA_REORDER = 1,
+
+
+
+
+)
 	(
-		input clk,
+		input i_clk,
+		input s_clk,
+		input i_rst,
+		input user_start,
+	
+
+
 		input empty,
 		input [47:0] data,
 		output rden
@@ -9,7 +89,7 @@ module rah_gati
 	reg [47:0] r_data=0;
 	reg valid_data=0;
 	
-	always @ (posedge clk) begin 
+	always @ (posedge i_clk) begin 
 		
 		if(!empty) begin 
 			rden<=1;
@@ -29,7 +109,43 @@ module rah_gati
 
 
 ////////////////////////////MIPI controller rx
-	
+top#(
+    .N_FIFO(N_FIFO),
+    .W_DATA(W_DATA),
+    .BURST_LEN(BURST_LEN),
+    .W_BURST_LEN(W_BURST_LEN),
+    .W_ADDR(W_ADDR),
+    .AXI_BYTES(AXI_BYTES)
+)(
+    .i_clk(i_clk),
+    .i_rstn (i_rstn),
+    .i_trigger(i_trigger),
+    .i_data_valid(i_data_valid),
+    .i_data(i_data),
+	.ddr_sel(ddr_sel),
+	.ddr_wready(ddr_wready),
+	.ddr_blen(ddr_blen),
+    .o_fifo_data(o_fifo_data),  //comes from fifo array
+    .final_o_data_last(final_o_data_last), //comes from dram wr ctrl
+    .o_data_valid(o_data_valid), //comes from dram wr ctrl
+    .req_wr_req_ctrl(req_wr_req_ctrl),
+    .address_wr_req_ctrl(address_wr_req_ctrl),
+    .final_burst_len_wr_req_ctrl(final_burst_len_wr_req_ctrl),
+    .final_last_wr_req_ctrl(final_last_wr_req_ctrl),
+    .valid_wr_req_ctrl(valid_wr_req_ctrl)
+);
+
+	wire [(W_DATA * N_FIFO)-1 : 0] o_fifo_data,  //comes from fifo array
+    wire final_o_data_last, //comes from dram wr ctrl
+    wire o_data_valid, //comes from dram wr ctrl
+    wire req_wr_req_ctrl,
+    wire [7:0] address_wr_req_ctrl,
+    wire [W_BURST_LEN-1 : 0] final_burst_len_wr_req_ctrl,
+    wire final_last_wr_req_ctrl,
+    wire valid_wr_req_ctrl
+
+
+
 //////////////////////////////
 	
 
@@ -38,81 +154,6 @@ module rah_gati
 	
 
 //////////////////////////////// gati module instatiation
-///globalutput [(OP_FIFO*DATA_WIDTH_OB)-1:0] op_dram_fifo
-	local  parameter INST_QUEUE_DEPTH = 512;
-	local  parameter DRAM_IMG_FIFO_DEPTH = 512;
-	local  parameter IM2COL_FIFO_DEPTH = 1024;
-	local  parameter WEIGHT_FIFO_DEPTH = 512;
-	local  parameter PSUM_FIFO_DEPTH = 8192;
-	local  parameter ACC_FIFO_DEPTH = 512;
-	local  parameter BIAS_FIFO_DEPTH = 512; //For both conv and FC
-	local  parameter OP_WRITE_FIFO_DEPTH = 2048;
-     //Default burst lenghts for various memory request controllers
-	local  parameter CONFIG_REQ_BLEN = 7;
-	local  parameter IMG_REQ_BLEN = 15;
-	local  parameter WEIGHT_REQ_BLEN = 15;
-   local  parameter ACC_REQ_BLEN = 15;
-   local  parameter OP_WRITE_REQ_ACC_BLEN = 31; //burst length for writng accumulants (32-bit) into the DRAM
-   local  parameter OP_WRITE_REQ_QUA_BLEN = 15; //burst length for writng quantized wire (8-bit) into the DRAM
-
-   local  parameter CONFIG_FIFO_OCCUPANCY = 10;
-   local  parameter LAYERCNT_WIDTH = 12;
-   local  //parameters related to DRAM controller
-   local  parameter NUM_PORTS = 4; //Number of read and write requestors
-   local  /*
-     parameter PORT_ID_WIDTH = 4; 
-     parameter ADDR_WIDTH_MEM_CTRL = 8; //address width to the wire of DRAM ctrl from operator mem request ctrlers
-     parameter PORT_ID = {4'd10; 4'd11; 4'd12; 4'd13; 4'd14} // Depends on the port_id_width and number of requestors
-     */
-
-   local  parameter AXI_DATA_BYTES = 32;  // Axi Data width = 256 bit
-   local  parameter AXI_ADDR_W = 32;          // Axi Address width
-   local  parameter BURST_LENGTH_WIDTH =8;
-   local 
-   local  parameter NUM_INSTRUCTIONS = 4;
-   local  parameter INST_W = 256;
-
-   local  parameter OPCODE_WIDTH = 4;
-   local  parameter N_SA = NSA_DSP + NSA_LUT;
-   local  parameter DATA_WIDTH = 8;
-   local  parameter COL_SA = 4;
-   local  parameter COL_FC = 32;
-   local  parameter W_QUANT_SHIFT = 5;
-   local  parameter W_QUANT_SCALE = 16;
-   local  parameter ROW = 9;
-   local  parameter W_PSUM = 20;
-   local  parameter MOD1 = 2;
-   local  parameter MOD2 = 8;
-   local  parameter DATA_WIDTH_OB = 32;
-   local  parameter IMAGE_DIM = 224;
-   local  parameter SHFT_REG_X = 4; // Number of shift register blocks
-   local  parameter BIAS_FIFO = 8; // Number of bias FIFOs
-   local  parameter OP_FIFO = 8;  // Number of wire write FIFOs
-   local  parameter NSA_DSP = 4; 
-   local  parameter N_FC_MUX = 4;
-   local  parameter NO_PORT_FC = 8;
-   local  parameter RELU_CLIP_WIDTH = 8;
-   local  parameter NSA_LUT = 0;
-   local  parameter BIAS_FIFO_FC = 32; // Number of FC bias FIFOs
-   local  parameter NO_PORT_VA = 2;
-   local  parameter NO_PORT_BAC = 2;
-   local  parameter NO_PORT_BAFC = 8;
-   local  parameter POP_THRESHOLD = 5;
-   local  parameter I_SIZE_WIDTH=20; // bit width of wire image dimension
-   local  parameter ACC_DW = 32;
-   local  parameter N_BANK = 4;
-   local  parameter N_BRAM = 8;
-   local  parameter W_FC_RW_COUNTER = 10; //width of fc r/w address counter
-   local  parameter FC_BRAM_DEPTH = 1024;
-   local  parameter W_KERNEL_CNT = 16;
-   local  parameter W_IMAG_DIM = 20;
-	local parameter ACC_DATA_REORDER = 1;
-	wire i_clk,
-    wire s_clk,
-    wire i_rst,
-    
-    ///////config block wire
-    wire user_start,
 
     //signals to DRAM ctrler
     ////config

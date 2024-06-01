f03cc6b611a349fcd8658ab48ce3b802a8f144fa
SHWET DHAMELIYA <shwet638@gmail.com>
2024-06-01 14:36:08 +0530

    typo fix

--- src/rtl/output_block/vector_mux_param.v
+++ src/rtl/output_block/vector_mux_param.v
@@ -1,7 +1,7 @@
 module vector_mux_param #(parameter PORT_SIZE=1'b1,
                      parameter NO_PORT=2  )
  (
-       input [PORT_SIZE*NO_PORT -1:0] in,
+       input [(PORT_SIZE*NO_PORT) -1:0] in,
     output wor  [PORT_SIZE-1:0] out,
         	 input[NO_PORT-1:0]  sel);
 

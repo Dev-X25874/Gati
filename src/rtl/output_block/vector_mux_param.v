516e5577c1fe8c8d0ad615472001dccb6b17cf9f
SHWET DHAMELIYA <shwet638@gmail.com>
2024-06-01 11:20:17 +0530

    changes

--- src/rtl/output_block/vector_mux_param.v
+++ src/rtl/output_block/vector_mux_param.v
@@ -1,5 +1,5 @@
-module vector_mux_param #(parameter PORT_SIZE=2,
-                     parameter NO_PORT=256  )
+module vector_mux_param #(parameter PORT_SIZE=1'b1,
+                     parameter NO_PORT=2  )
  (
        input [PORT_SIZE*NO_PORT -1:0] in,
     output wor  [PORT_SIZE-1:0] out,

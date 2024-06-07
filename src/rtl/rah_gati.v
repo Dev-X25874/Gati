52fb5b9b185ad6f3957da7d2bf10d134d09175c6
SHWET DHAMELIYA <shwet638@gmail.com>
2024-06-07 11:21:14 +0530

    t  clk

--- src/rtl/rah_gati.v
+++ src/rtl/rah_gati.v
@@ -137,6 +137,7 @@ module rah_gati #(
     input i_clk,
     input s_clk,
     input m_clk,
+	input t_clk,
 	input i_rst,
     input empty,
     input [31:0] data,
@@ -195,7 +196,7 @@ assign o_rden=rden;
     end
 end 
 
-	always @(posedge m_clk) begin 
+	always @(posedge t_clk) begin 
 		 if (o_rden)  begin 
 			  valid_data <= 1;
 			 
@@ -248,7 +249,7 @@ end
       .W_ADDR(IN_ADDR),
       .AXI_BYTES(AXI_DATA_BYTES)
   ) mipi_ctrler_reciver (
-      .i_clk(m_clk),
+      .i_clk(t_clk),
       .i_rstn(i_rst),
       .i_data_valid(valid_data),
       .i_data(o_data),

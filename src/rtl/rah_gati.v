6e0bf33ee18a68d215a16b6831866955b52fc674
SHWET DHAMELIYA <shwet638@gmail.com>
2024-06-07 12:05:17 +0530

    changes

--- src/rtl/rah_gati.v
+++ src/rtl/rah_gati.v
@@ -137,7 +137,6 @@ module rah_gati #(
     input i_clk,
     input s_clk,
     input m_clk,
-	input xy_clk,
 	input i_rst,
     input empty,
     input [31:0] data,
@@ -187,7 +186,7 @@ module rah_gati #(
   wire o_rden;
 assign o_rden=rden;
 
-  always @(posedge xy_clk) begin
+  always @(posedge  m_clk) begin
 
     if (!empty) begin
       rden <= 1;
@@ -196,7 +195,7 @@ assign o_rden=rden;
     end
 end 
 
-	always @(posedge xy_clk) begin 
+	always @(posedge  m_clk) begin 
 		 if (o_rden)  begin 
 			  valid_data <= 1;
 			 
@@ -249,7 +248,7 @@ end
       .W_ADDR(IN_ADDR),
       .AXI_BYTES(AXI_DATA_BYTES)
   ) mipi_ctrler_reciver (
-      .i_clk(xy_clk),
+      .i_clk(m_clk),
       .i_rstn(i_rst),
       .i_data_valid(valid_data),
       .i_data(o_data),

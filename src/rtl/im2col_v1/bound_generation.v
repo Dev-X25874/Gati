7d28555ab96ef3b9c5f0a67050002558bcdbb29d
Kasetty-Praveen-Kumar <kpkkumar.1991@gmail.com>
2025-08-23 09:54:50 +0530

    Fix(Im2col bound generation): Fixes in bound generation logic in im2col
     * To reduce the resource utilization, bound generation logic is shared
       between valid squares generation and mod operator to handle stride.
     * An additional condition was missing for the row FIFOs of SA if ROW >
       kw*kh. (ROW - design parameter of SA)
    
    Signed-off-by: Kasetty-Praveen-Kumar <kpkkumar.1991@gmail.com>

--- src/rtl/im2col_v1/bound_generation.v
+++ src/rtl/im2col_v1/bound_generation.v
@@ -5,7 +5,7 @@ module bound_generation_v1 #(
   parameter CONV_KW_WIDTH = 4,
   parameter CONV_KH_WIDTH = 4,
   parameter ROW = 9)
-  (
+(
   input                               i_valid,
   input  [(UPPER_BOUND)-1:0]          mat_size_col,
   input  [(UPPER_BOUND)-1:0]          mat_size_row,     
@@ -49,37 +49,43 @@ module bound_generation_v1 #(
   reg [$clog2(ROW) : 0] i,j,k;
   always@(posedge clk) begin
     if(!rstn) begin
-      i <= 0;
-      j <= 0;
-      k <= 0;
-      bound_gen_done_row <= 0;
-      lower_bound_row <= 0;
-    end
-    else begin
-      if(start_SA) begin 
-        bound_gen_done_row <= 0;
         i <= 0;
         j <= 0;
         k <= 0;
+        bound_gen_done_row <= 0;
         lower_bound_row <= 0;
-      end
-      else if (!bound_gen_done_row) begin
-        if(i<kw*kh) begin
-          lower_bound_row[(DATA_WIDTH*(ROW-i))-1 -:DATA_WIDTH] <= k+1;
-          i <= i+1;
-          if(j<kw-1) begin
-            j <= j+1;
-            k <= k;
-          end
-          else begin
+    end
+    else begin
+        if(start_SA) begin 
+            bound_gen_done_row <= 0;
+            i <= 0;
             j <= 0;
-            k <= k+1;
-          end
+            k <= 0;
+            lower_bound_row <= 0;
         end
-        else begin
-          bound_gen_done_row <= 1;
+        else if (!bound_gen_done_row) begin
+            if(i<kw*kh) begin
+                lower_bound_row[(DATA_WIDTH*(ROW-i))-1 -:DATA_WIDTH] <= k+1;
+                i <= i+1;
+                if(j<kw-1) begin
+                    j <= j+1;
+                    k <= k;
+                end
+                else begin
+                    j <= 0;
+                    k <= k+1;
+                end
+            end
+            else begin
+                if(i == ROW) begin
+                    bound_gen_done_row <= 1;
+                end
+                else begin
+                    lower_bound_row[(DATA_WIDTH*(ROW-i))-1 -:DATA_WIDTH] <= lower_bound_row[(DATA_WIDTH*(ROW-((kw*kh)-1)))-1 -:DATA_WIDTH];
+                    i <= i+1;
+                end
+            end
         end
-      end
     end
   end
  
@@ -107,7 +113,13 @@ module bound_generation_v1 #(
                 else m <= 0;
             end
             else begin
-                bound_gen_done_col <= 1;
+                if(l == ROW) begin
+                    bound_gen_done_col <= 1;
+                end
+                else begin
+                    lower_bound_col[(DATA_WIDTH*(ROW-l))-1 -:DATA_WIDTH] <= lower_bound_col[(DATA_WIDTH*(ROW-((kw*kh)-1)))-1 -:DATA_WIDTH];
+                    l <= l+1;
+                end
             end
         end
     end
@@ -126,48 +138,48 @@ module bound_generation_v1 #(
 integer sq;
 
 always @(posedge clk) begin
-  if(!rstn) begin
-    valid_sq_reg <= 0;
-    r_data_i <= 0;
-  end
-  else if ( rstn && ~i_stall_on) begin // checking for each of the bounds
-    for(sq=0; sq<ROW; sq=sq+1) begin
-      if(sq<(kw*kh)) begin
-          if (((row >= lower_bound_row[(DATA_WIDTH*(ROW-sq))-1 -:DATA_WIDTH]) && (col >= lower_bound_col[(DATA_WIDTH*(ROW-sq))-1 -:DATA_WIDTH])) && (((row) <= (upper_bound_row[(DATA_WIDTH*(ROW-sq))-1 -:DATA_WIDTH]) && ((col) <= (upper_bound_col[(DATA_WIDTH*(ROW-sq))-1 -:DATA_WIDTH]))))) begin
-              valid_sq_reg[sq] <= 1;   // valid_sq is assigned 1 according to the bounds the coordinate falls in
-          end
-          else begin 
-              valid_sq_reg[sq] <= 0;
-          end
-      end
-      else begin
-        if (((row >= lower_bound_row[(DATA_WIDTH*(ROW-((kh*kw)-1)))-1 -:DATA_WIDTH]) && (col >= lower_bound_col[(DATA_WIDTH*(ROW-((kh*kw)-1)))-1 -:DATA_WIDTH])) && (((row) <= (upper_bound_row[(DATA_WIDTH*(ROW-((kh*kw)-1)))-1 -:DATA_WIDTH]) && ((col) <= (upper_bound_col[(DATA_WIDTH*(ROW-((kh*kw)-1)))-1 -:DATA_WIDTH]))))) begin
-          valid_sq_reg[sq] <= 1;   // valid_sq is assigned 1 according to the bounds the coordinate falls in
-      end
-      else begin 
-          valid_sq_reg[sq] <= 0;
-      end
-      end
+    if(!rstn) begin
+        valid_sq_reg <= 0;
+        r_data_i <= 0;
     end
-  end
+    else if ( rstn && ~i_stall_on) begin // checking for each of the bounds
+        for(sq=0; sq<ROW; sq=sq+1) begin
+          if(sq<(kw*kh)) begin
+            if (((row >= lower_bound_row[(DATA_WIDTH*(ROW-sq))-1 -:DATA_WIDTH]) && (col >= lower_bound_col[(DATA_WIDTH*(ROW-sq))-1 -:DATA_WIDTH])) && (((row) <= (upper_bound_row[(DATA_WIDTH*(ROW-sq))-1 -:DATA_WIDTH]) && ((col) <= (upper_bound_col[(DATA_WIDTH*(ROW-sq))-1 -:DATA_WIDTH]))))) begin
+                valid_sq_reg[sq] <= 1;   // valid_sq is assigned 1 according to the bounds the coordinate falls in
+            end
+            else begin 
+                valid_sq_reg[sq] <= 0;
+            end
+          end
+          else begin
+            if (((row >= lower_bound_row[(DATA_WIDTH*(ROW-((kh*kw)-1)))-1 -:DATA_WIDTH]) && (col >= lower_bound_col[(DATA_WIDTH*(ROW-((kh*kw)-1)))-1 -:DATA_WIDTH])) && (((row) <= (upper_bound_row[(DATA_WIDTH*(ROW-((kh*kw)-1)))-1 -:DATA_WIDTH]) && ((col) <= (upper_bound_col[(DATA_WIDTH*(ROW-((kh*kw)-1)))-1 -:DATA_WIDTH]))))) begin
+                valid_sq_reg[sq] <= 1;   // valid_sq is assigned 1 according to the bounds the coordinate falls in
+            end
+            else begin 
+                valid_sq_reg[sq] <= 0;
+            end
+          end
+        end
+    end
 
-  // make's valid square zero if the stall is one 
-  else if (i_stall_on) begin 
-    valid_sq_reg <= 0; 
-  end 
+    // make's valid square zero if the stall is one 
+    else if (i_stall_on) begin 
+        valid_sq_reg <= 0; 
+    end 
 end
 
 
 //Generation of im2col_start_flag
 reg r_im2col_start_flag = 0;
 always@(posedge clk) begin
-  if(!rstn) begin
-    r_im2col_start_flag <= 0;
-  end
-  else begin
-    if(im2col_start) r_im2col_start_flag <= 1;
-    else if(im2col_done) r_im2col_start_flag <= 0;
-  end
+    if(!rstn) begin
+        r_im2col_start_flag <= 0;
+    end
+    else begin
+        if(im2col_start) r_im2col_start_flag <= 1;
+        else if(im2col_done) r_im2col_start_flag <= 0;
+    end
 end
 
 assign o_valid = i_valid;
--- src/rtl/im2col_v1/stride_block.v
+++ src/rtl/im2col_v1/stride_block.v
@@ -21,95 +21,12 @@ module stride_block_v1 #(
 
     wire [(UPPER_BOUND)-1:0] row;
     wire [(UPPER_BOUND)-1:0] col;
-    // reg [(DATA_WIDTH*ROW)-1:0]           lower_bound_row = 0;
-    // reg [(DATA_WIDTH*ROW)-1:0]           lower_bound_col = 0;
-    reg                                  bound_gen_done_row = 0; 
-    reg                                  bound_gen_done_col = 0;
+    
+    reg bound_gen_done_row = 0; 
+    reg bound_gen_done_col = 0;
     
     assign row = curr_row;
     assign col = curr_col;
-
-    /*
-    // Generation of lower_bound_row
-    reg [$clog2(ROW) : 0] i,j,k;
-    always@(posedge clk) begin
-      if(!rst) begin
-          i <= 0;
-          j <= 0;
-          k <= 0;
-          bound_gen_done_row <= 0;
-          lower_bound_row <= 0;
-      end
-      else begin
-          if(start_SA) begin 
-              bound_gen_done_row <= 0;
-              i <= 0;
-              j <= 0;
-              k <= 0;
-              lower_bound_row <= 0;
-          end
-          else if (!bound_gen_done_row) begin
-              if(i<kw*kh) begin
-                  lower_bound_row[(DATA_WIDTH*(ROW-i))-1 -:DATA_WIDTH] <= k+1;
-                  i <= i+1;
-                  if(j<kw-1) begin
-                    j <= j+1;
-                    k <= k;
-                  end
-                  else begin
-                    j <= 0;
-                    k <= k+1;
-                  end
-              end
-              else begin
-                if(i == ROW) begin
-                    bound_gen_done_row <= 1;
-                end
-                else begin
-                    lower_bound_row[(DATA_WIDTH*(ROW-i))-1 -:DATA_WIDTH] <= lower_bound_row[(DATA_WIDTH*(ROW-((kw*kh)-1)))-1 -:DATA_WIDTH];
-                    i <= i+1;
-                end
-              end
-          end
-      end
-    end
-  
-    // Generation of lower_bound_col
-    reg [$clog2(ROW) : 0] l,m;
-    always@(posedge clk) begin
-      if(!rst) begin
-          l <= 0;
-          m <= 0;
-          bound_gen_done_col <= 0;
-          lower_bound_col <= 0;
-      end
-      else begin
-          if(start_SA) begin 
-              bound_gen_done_col <= 0;
-              l <= 0;
-              m <= 0;
-              lower_bound_col <= 0;
-          end
-          else if (!bound_gen_done_col) begin
-              if(l<kw*kh) begin
-                  lower_bound_col[(DATA_WIDTH*(ROW-l))-1 -:DATA_WIDTH] <= m+1;
-                  l <= l+1;
-                  if(m<kw-1) m <= m+1;
-                  else m <= 0;
-              end
-              else begin
-                if(l == ROW) begin
-                    bound_gen_done_col <= 1;
-                end
-                else begin
-                    lower_bound_col[(DATA_WIDTH*(ROW-l))-1 -:DATA_WIDTH] <= lower_bound_col[(DATA_WIDTH*(ROW-((kw*kh)-1)))-1 -:DATA_WIDTH];
-                    l <= l+1;
-                end
-              end
-          end
-      end
-    end
-    */
 
     // Stride block for each valid_sq row
     genvar sq;
